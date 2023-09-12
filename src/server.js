a23ed4e638893c52a74c86ad09c27718eb633b97
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2023-09-12 17:09:08 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended:false}))
 // app.use(bodyParser.json())
 app.use(cors({
-    origin: ['http://localhost:3002','https://parach-mgt.vercel.app'],
+    origin: ['http://localhost:3002','https://mim-ruddy.vercel.app'],
     credentials:true
 }))
 
